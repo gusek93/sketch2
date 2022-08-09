9f1cde1635ad2d1f7c948531dd7f5dd68ac88c3b
Dayong Hyun <46876637+gusek93@users.noreply.github.com>
2022-08-09 20:32:48 +0900

    project init settings

--- src/main/java/com/example/sketch2/Sketch2Application.java
+++ src/main/java/com/example/sketch2/Sketch2Application.java
@@ -1,13 +1,16 @@
 package com.example.sketch2;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@Slf4j
 @SpringBootApplication
 public class Sketch2Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Sketch2Application.class, args);
+        log.info("http://localhost:8080/graphiql");
     }
 
 }
--- src/main/java/com/example/sketch2/graphqlTest/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sketch2.graphqlTest.repository;
-
-import com.example.sketch2.graphqlTest.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Integer> {
-}
