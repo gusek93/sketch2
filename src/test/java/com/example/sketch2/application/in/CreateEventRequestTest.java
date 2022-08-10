7affba487aada2601dc7758871b5cfd80bdb5b7e
Dayong Hyun <46876637+gusek93@users.noreply.github.com>
2022-08-10 18:14:25 +0900

    (test failed) CreateEventRequest of

---
+++ src/test/java/com/example/sketch2/application/in/CreateEventRequestTest.java
@@ -0,0 +1,17 @@
+package com.example.sketch2.application.in;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateEventRequestTest {
+    @Test
+    void of() {
+        final var request = CreateEventRequest.of(1, "2022-12-25 12:34:56");
+
+        assertThat(LocalDateTime.of(2022,12,25,34,56));
+    }
+}
