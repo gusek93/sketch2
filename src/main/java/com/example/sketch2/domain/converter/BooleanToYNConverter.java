ecda4b968b3d9a4220350d2fa9275123f414d904
Dayong Hyun <46876637+gusek93@users.noreply.github.com>
2022-08-11 16:28:53 +0900

    BooleanToYNConverter add

---
+++ src/main/java/com/example/sketch2/domain/converter/BooleanToYNConverter.java
@@ -0,0 +1,16 @@
+package com.example.sketch2.domain.converter;
+
+import javax.persistence.AttributeConverter;
+
+public class BooleanToYNConverter implements AttributeConverter<Boolean, String> {
+
+    @Override
+    public String convertToDatabaseColumn(Boolean attribute) {
+        return attribute ? "Y" : "N";
+    }
+
+    @Override
+    public Boolean convertToEntityAttribute(String dbData) {
+        return "Y".equalsIgnoreCase(dbData);
+    }
+}
