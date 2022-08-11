467f15194cbae69b5b22153ecfb21e62d232fb4f
Dayong Hyun <46876637+gusek93@users.noreply.github.com>
2022-08-11 11:07:06 +0900

    add oeverloaded method

--- src/main/java/com/example/sketch2/application/out/CreateEventOutPort.java
+++ src/main/java/com/example/sketch2/application/out/CreateEventOutPort.java
@@ -2,6 +2,10 @@ package com.example.sketch2.application.out;
 
 import com.example.sketch2.domain.OnlineEvent;
 
+import java.util.List;
+
 public interface CreateEventOutPort {
     void create(OnlineEvent onlineEvent);
+
+    void create(List<OnlineEvent> onlineEvents);
 }
